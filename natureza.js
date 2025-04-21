94ed02c41d7f780bde77cb7bb3c1d07556d0f237
biadjordjevic <biadjordjevic@gmail.com>
2025-04-21 00:03:11 -0300

    Alterações

---
+++ natureza.js
@@ -0,0 +1,16 @@
+    function abrirModal(id) {
+    document.getElementById(id).style.display = "flex";
+  }
+
+  function fecharModal(id) {
+    document.getElementById(id).style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    const modais = document.querySelectorAll(".modal");
+    modais.forEach(function(modal) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  };
